ebec6e0be8a6fb0a5ef97efc16afc454a7a12fe4
Jonathan Arnett <jonarnett90@gmail.com>
2024-02-04 11:57:16 -0500

    Convert app to "modern" (hooks) style React

--- App.jsx
+++ App.jsx
@@ -1,60 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Parser from 'web-tree-sitter';
+
+/* Language modules */
 import * as elixir from './elixir';
+
 import './App.css';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
+export default function App() {
+    const [code, setCode] = useState(undefined);
+    const [languageMod, setLanguageMod] = useState(elixir);
+    const [parser, setParser] = useState(undefined);
+    const [tree, setTree] = useState(undefined);
 
-        this.state = {
-            code: undefined,
-            languageMod: undefined,
-            parser: undefined,
-            tree: undefined
-        };
-    }
+    const focusTextarea = () => {
+        document.querySelector('textarea').focus();
+    };
 
-    componentDidMount() {
+    /* First render stuff */
+    useEffect(() => {
         Parser.init({
             locateFile(scriptName, _scriptDirectory) {
                 return scriptName;
-            }
-        }).then(() => this.setState({parser: new Parser()}))
-          .then(() => this.initializeParser(elixir));
+            },
+        }).then(() => setParser(new Parser()));
+
+        focusTextarea();
+    }, []);
+
+    useEffect(() => {
+        const loadLanguage = async () => {
+            const language = await Parser.Language.load(languageMod.treeSitterWasm);
+            const code = languageMod.defaultProgram;
+
+            const parser = new Parser();
+            parser.setLanguage(language);
+            const tree = parser.parse(code);
+
+            setParser(parser);
+            setCode(code);
+            setTree(tree);
+            setLanguageMod(languageMod);
+        };
+        loadLanguage();
+    }, [languageMod]);
+
+    const updateContent = (event) => {
+        setCode(event.target.value);
+        setTree(parser.parse(event.target.value));
     }
 
-    async initializeParser(languageMod) {
-        const language = await Parser.Language.load(languageMod.treeSitterWasm);
-        this.state.parser.setLanguage(language);
-        const code = languageMod.defaultProgram;
-        const tree = this.state.parser.parse(code);
-
-        this.setState({ code, tree, languageMod });
-    }
-
-    updateContent(event) {
-        this.setState({ code: event.target.value, tree: this.state.parser.parse(this.state.code) });
-    }
-
-    focusTextarea() {
-        document.querySelector("textarea").focus();
-    }
-
-    placeCursor(code, cursorPos) {
+    /* Meant for when the color view is directly editable; not currently used. */
+    const placeCursor = (code, cursorPos) => {
         const before = code.slice(0, cursorPos);
         const beginAfter = code.slice(cursorPos, cursorPos + 1) === "\n" ? cursorPos : cursorPos + 1;
         const after = code.slice(beginAfter);
         return (before + "█" + after);
     }
 
-    renderCode(text, cursor) {
+    const determineStyle = (cursor) => {
+        let color = languageMod.colorMapping[cursor.nodeType];
+        let border;
+        let colored = true;
+
+        if (color === undefined) {
+            color = "rgba(0,0,0,0)";
+            border = "rgba(0,0,0,0)";
+            colored = false;
+        } else {
+            const [h, s, l] = color;
+            const borderLightness = l - 10 < 0 ? 0 : l - 10;
+            color = `hsl(${h}deg, ${s}%, ${l}%)`;
+            border = `hsl(${h}deg, ${s}%, ${borderLightness}%)`;
+        }
+
+        return [colored, { backgroundColor: color, borderColor: border }];
+    }
+
+    const renderCode = (text, cursor) => {
         let childCodes = [];
         let lastEnd = cursor.startIndex;
 
         if (cursor.gotoFirstChild()) {
             do {
-                let [childStart, childEnd, childRendered] = this.renderCode(text, cursor);
+                let [childStart, childEnd, childRendered] = renderCode(text, cursor);
 
                 if (childStart > lastEnd) {
                     childCodes.push(<>{text.slice(lastEnd, childStart)}</>);
@@ -73,7 +101,7 @@ class App extends React.Component {
         }
 
         const key = window.crypto.randomUUID();
-        const [colored, style] = this.determineStyle(cursor);
+        const [colored, style] = determineStyle(cursor);
         const className = colored ? 'colored' : '';
 
         let attrs = { key, style, className }
@@ -84,41 +112,16 @@ class App extends React.Component {
         return [cursor.startIndex, cursor.endIndex, <div {...attrs}>{childCodes}</div>];
     }
 
-    determineStyle(cursor) {
-        let color = this.state.languageMod.colorMapping[cursor.nodeType];
-        let border;
-        let colored = true;
+    let [_start, _end, renderedCode] = tree ? renderCode(code, tree.walk()) : [<></>];
 
-        if (color === undefined) {
-            color = "rgba(0,0,0,0)";
-            border = "rgba(0,0,0,0)";
-            colored = false;
-        } else {
-            const [h, s, l] = color;
-            const borderLightness = l - 10 < 0 ? 0 : l - 10;
-            color = `hsl(${h}deg, ${s}%, ${l}%)`;
-            border = `hsl(${h}deg, ${s}%, ${borderLightness}%)`;
-        }
-
-        return [colored, { backgroundColor: color, borderColor: border }];
-    }
-
-    render() {
-        let [_start, _end, renderedCode] = (this.state.tree) ? this.renderCode(this.state.code, this.state.tree.walk()) : [<></>];
-
-        return (
-            <div className="App" >
-                <textarea
-                    onKeyUp={(e) => this.updateContent(e)}
-                    onKeyDown={(e) => this.updateContent(e)}
-                    //onChange={(e) => this.updateContent(e)}
-                    defaultValue={this.state.code}></textarea>
-                <div className="editor" onClick={() => this.focusTextarea()}>
-                    {renderedCode}
-                </div>
+    return (
+        <div className="App" >
+            <textarea
+                onKeyUp={(e) => updateContent(e)}
+                defaultValue={code}></textarea>
+            <div className="editor" onClick={() => this.focusTextarea()}>
+                {renderedCode}
             </div>
-        );
-    }
+        </div>
+    );
 }
-
-export default App;
