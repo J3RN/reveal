d7d20b7a13c596683411e3839258dc88462ffba5
Jonathan Arnett <jonarnett90@gmail.com>
2024-02-05 11:04:58 -0500

    Normalize saturation and lightness

--- javascript.js
+++ javascript.js
@@ -1,14 +1,14 @@
 export const colorMapping = {
-  export_statement:     [123, 45, 92],
-  arrow_function:       [270, 45, 90],
-  function_declaration: [270, 45, 90],
-  call_expression:      [ 56, 19, 98],
-  formal_parameters:    [311, 20, 95],
-  arguments:            [311, 20, 95],
-  string:               [195, 41, 96],
-  number:               [180, 26, 90],
-  identifier:           [ 27, 18, 99],
-  lexical_declaration:  [350, 40, 90]
+  export_statement:     [123, 50, 90],
+  arrow_function:       [270, 50, 90],
+  function_declaration: [270, 50, 90],
+  call_expression:      [ 56, 50, 90],
+  formal_parameters:    [311, 50, 90],
+  arguments:            [311, 50, 90],
+  string:               [195, 50, 90],
+  number:               [180, 50, 90],
+  identifier:           [ 27, 50, 90],
+  lexical_declaration:  [350, 50, 90]
 };
 
 export const defaultProgram =
