e56738d090b9c3375cbf1332fb803c276b10abfb
Jonathan Arnett <jonarnett90@gmail.com>
2024-02-15 16:01:22 -0500

    Various node/color extensions

--- common_colors.js
+++ common_colors.js
@@ -1,11 +1,20 @@
+const collection = 210;
+
 const commonColors = {
   ERROR:       0,
   identifier:  15,
+  property:    30,
   declaration: 45,
   call:        60,
   block:       90,
+  import:      120,
+  control:     135,
   primitive:   180,
-  collection:  210,
+  array:       collection,
+  collection:  collection,
+  list:        collection,
+  string:      collection,
+  tuple:       collection,
   comment:     225,
   function:    270,
   parameters:  315,
